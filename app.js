4f42d03b9d82a5c002b666ff6cedd7709461b235
stanciu9877 <48034236+stanciu9877@users.noreply.github.com>
2021-05-17 19:23:29 +0300

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector(".nav-trigger");
-const links = document.querySelector(".link-uri");
-
-navToggle.addEventListener("click", function () {
-  links.classList.toggle("show-links");
-});
